d771f2be0953b733b6453bb3924bf55194649a55
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-08-03 11:07:24 -0400

    abstracted session use to sessionController and cleaned code

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,4 +1,5 @@
 const Session = require('../models/sessionModel')
+const { v4: uuidv4 } = require('uuid');
 
 const findUser = async (sessionToken) => {
     let session;
@@ -7,15 +8,70 @@ const findUser = async (sessionToken) => {
     } catch (error){
         console.error("Error finding username for session:", error);
     }
-    if (session === null){
-        //TODO
-        return null;
-    } else {
-        return session.username;
-    }
-    
+    return session ? session.username : null;
 }
 
+const findExistingSession = async (username) => {
+    try {
+        return await Session.findOne({ username: username });
+    } catch (error) {
+        console.error("Error checking for existing username:", error);
+        throw error;
+    }
+};
+
+const deactivateSession = async (sessionToken) => {
+    if (sessionToken) {
+        try {
+            await Session.findOneAndUpdate(
+                { token: sessionToken },
+                { isActive: false },
+                { new: true }
+            );
+            console.log(`Session ${sessionToken} set to inactive`);
+        } catch (error) {
+            console.error('Error setting session to inactive:', error);
+        }
+    }
+}
+
+const updateSession = async (session, updateData) => {
+    try {
+        Object.keys(updateData).forEach(key => {
+            if (session.schema.path(key)) {
+                session[key] = updateData[key];
+            }
+        });
+        await session.save();
+    } catch (error) {
+        console.error('Error updating session data:', error);
+        throw error;
+    }
+};
+
+const createSession = async (username) => {
+    try {
+        // Create a new session token
+        const token = uuidv4();
+
+        // Give them username
+        const session = new Session({
+            token: token,
+            username: username
+        });
+        await session.save();
+        return token;
+    } catch (error) {
+        console.error("Error creating new session:", error);
+        throw error;
+    }
+};
+
+
 module.exports = {
-    findUser
+    findUser,
+    findExistingSession,
+    deactivateSession,
+    updateSession,
+    createSession
 };
--- src/middleware/usernameMiddleware.js
+++ src/middleware/usernameMiddleware.js
@@ -1,20 +1,4 @@
-const Session = require('../models/sessionModel')
-const { v4: uuidv4 } = require('uuid');
-
-async function updateSession(session, updateData){
-    try {
-        Object.keys(updateData).forEach(key => {
-            if (session.schema.path(key)) {
-                session[key] = updateData[key];
-            }
-        });
-
-        await session.save();
-    } catch (error) {
-        console.error('Error updating session data:', error);
-        throw error
-    }
-}
+const sessionController = require('../controllers/sessionController');
 
 const usernameMiddleware = async (req, res, next) => {
     try {
@@ -23,56 +7,30 @@ const usernameMiddleware = async (req, res, next) => {
         console.log('Username:', username);
         console.log('Client Token:', clientToken);
         
-        // Get info on existing session with username
-        let existingSession;
-        try {
-            existingSession = await Session.findOne({ username: username });
-        } catch (error) {
-            console.error("Error checking for existing username:", error);
-            return res.status(500).json({ locationValid: true, usernameValid: false });
-        }
+        // Get existing session from username
+        const existingSession = await sessionController.findExistingSession(username);
 
         // If it doesn't exist
         if (existingSession === null){
-            try {
-                // Create a new session token
-                const token = uuidv4();
-
-                // Give them that username
-                const session = new Session({
-                    token: token,
-                    username: username
-                });
-                await session.save();
-
-                // Send down the line
-                console.log("Valid Username:", username);
-                console.log("SessionToken:", token);
-                req.sessionToken = token;
-                return next();
-            } catch (error) {
-                console.error("Error creating new session:", error);
-                return res.status(500).json({ locationValid: true, usernameValid: false });
-            }
+            // Create session
+            const token = await sessionController.createSession(username);
+            // Send down the line
+            console.log("Valid Username:", username);
+            console.log("SessionToken:", token);
+            req.sessionToken = token;
+            return next();
         } else if (!existingSession.isActive && existingSession.token === clientToken) { // If inactive and matching token recover
-            try {
-                // Keep same token
-                const token = clientToken
-
-                // Reactivate the session
-                await updateSession(existingSession, {
-                    isActive: true
-                });
-
-                // Send down the line
-                console.log("Valid Recovery: ", username)
-                console.log("SessionToken:", token);
-                req.sessionToken = token;
-                return next();
-            } catch (error) {
-                console.error("Error recovering session:", error);
-                return res.status(500).json({ locationValid: true, usernameValid: false });
-            }
+            // Reactivate the session
+            await sessionController.updateSession(existingSession, {
+                isActive: true
+            });
+            
+            // Send down the line
+            console.log("Valid Recovery: ", username)
+            console.log("SessionToken:", clientToken);
+            // Keep token
+            req.sessionToken = clientToken;
+            return next();
         } else { // Username is already in use
             console.log("Invalid Username")
             return res.status(403).json({ locationValid: true, usernameValid: false });
