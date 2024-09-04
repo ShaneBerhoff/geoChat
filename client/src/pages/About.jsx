1d13f70d415af33a3946b1484b10c80ff837a9a5
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-09-03 20:35:54 -0400

    Initial commit for branch

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 <div className="columns">
                     <div>
                         <h2><strong>What is geoChat?</strong></h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p>geoChat is a collection of entirely anonymous live chatrooms.</p>
                     </div>
 
                     <div>
@@ -23,11 +23,6 @@ const About = () => {
 
                     <div>
                         <h2><strong>How It Works</strong></h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    </div>
-
-                    <div>
-                        <h2><strong>What is geoChat?</strong></h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     </div>
                 </div>
