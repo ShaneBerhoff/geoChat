80645509537e2d1ae1f8373ed1ff7749e8b17714
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-09-14 02:08:09 -0400

    schema for zones and room

--- server/models/sessionModel.js
+++ server/models/sessionModel.js
@@ -2,39 +2,51 @@ const { Schema, model } = require('mongoose');
 
 const SESSION_RECOVERY_MS = parseInt(process.env.SESSION_RECOVERY_PERIOD) * 60 * 1000;
 
+const chatRoomSchema = new Schema({
+    id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['zone', 'room'],
+        required: true
+    }
+}, { _id: false });
+
 const sessionSchema = new Schema({
-    token: { 
-        type: String, 
-        required: true, 
-        unique: true 
+    token: {
+        type: String,
+        required: true,
+        unique: true
     },
-    username: { 
-        type: String, 
-        required: true 
+    username: {
+        type: String,
+        required: true
     },
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
+    createdAt: {
+        type: Date,
+        default: Date.now
     },
-    isActive: { 
-        type: Boolean, 
-        default: false 
+    isActive: {
+        type: Boolean,
+        default: false
     },
     expiresAt: {
         type: Date,
-        default: function(){
+        default: function () {
             return new Date(Date.now() + SESSION_RECOVERY_MS);
         },
         expires: 0
     },
-    campus: {
-        type: Schema.Types.ObjectId,
-        ref: 'Campus'
-    },
-    building: {
-        type: Schema.Types.ObjectId,
-        ref: 'Building'
-    }
+    chatRooms: {
+        type: [chatRoomSchema],
+        default: []
+      }
 });
 
 sessionSchema.index({ token: 1 });
