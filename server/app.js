c9114d8c6fc797d7f17d8c84944dc7c5c2d290c4
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-10-22 16:16:06 -0400

    limits and security added to api routes

--- server/app.js
+++ server/app.js
@@ -1,25 +1,50 @@
-const express = require('express');
-const staticRoutes = require('./routes/staticRoutes');
-const apiRoutes = require('./routes/apiRoutes');
-const cors = require('cors');
-const corsOptions = require('./utils/corsOptions');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const staticRoutes = require("./routes/staticRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-    // In production, trust the reverse proxy
-    app.set('trust proxy', 1);
-} else {
-    // In dev use cors
-    app.use(cors(corsOptions));
+
+// Security headers
+app.use(helmet());
+
+// rate limits
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+  message: "Too many requests from this IP, please try again later",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 1, // allow 40 requests per 15 minutes, then...
+  delayMs: () => 500, // begin adding 500ms of delay per request
+});
+
+if (process.env.NODE_ENV === "production") {
+  // In production, trust the reverse proxy
+  app.set("trust proxy", 1);
 }
-app.use(express.json());
+
+app.use(express.json({ limit: "10kb" })); // Limit body size
 app.use(cookieParser());
 
-// API routes
-app.use('/api', apiRoutes)
+// apply limits
+app.use("/api/", apiLimiter);
+app.use("/api/", speedLimiter);
 
-// Static files and react routing
-app.use('/', staticRoutes)
+// ROUTES
+app.use("/api", apiRoutes); // API routes
+app.use("/", staticRoutes); // Static files and react routing
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
 
 module.exports = app;
